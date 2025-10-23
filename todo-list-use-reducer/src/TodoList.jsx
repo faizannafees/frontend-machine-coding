f2753a10277aa23956cbe77597508ed6e7c6558b
Faizan Nafees <faizannafees0@gmail.com>
2025-10-23 16:40:53 +0530

    todo-list-with-useReducer

---
+++ todo-list-use-reducer/src/TodoList.jsx
@@ -0,0 +1,108 @@
+import { useReducer, useState } from 'react'
+import './App.css'
+import { Edit, Trash } from 'lucide-react'
+
+const TYPES = {
+  ADD: 'ADD',
+  EDIT: 'EDIT',
+  DELETE: 'DELETE',
+  TOGGLE: 'TOGGLE'
+}
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case TYPES.ADD:
+      return [...state, { id: Date.now(), text: action.text, done: false }]
+
+    case TYPES.EDIT:
+      return state.map(todo => (
+        todo.id === action.id ? {...todo, text: action.text} : todo
+      ))
+
+    case TYPES.DELETE:
+      return state.filter(todo => todo.id !== action.id)
+
+    case TYPES.TOGGLE:
+      return state.map(todo => todo.id === action.id ? { ...todo, done: !todo.done } : todo )
+
+    default:
+      return state;
+  }
+
+}
+
+function TodoList() {
+
+  const [todoList, dispatch] = useReducer(todoReducer, []);
+  const [text, setText] = useState("");
+
+  const [currentEditedId, setCurrentEditedId] = useState(null);
+  const [currentEditedText, setCurrentEditedText] = useState("")
+
+  const addTodo = () => {
+    if (text.trim()) {
+      dispatch(
+        {
+          type: TYPES.ADD,
+          text: text.trim()
+        }
+      )
+      setText("");
+    }
+  }
+
+  // console.log(todoList);
+
+  const saveEdit = () => {
+    if(currentEditedText.trim()){
+      dispatch({
+        type: TYPES.EDIT,
+        id: currentEditedId,
+        text: currentEditedText.trim()
+      })
+      setCurrentEditedId(null)
+      setCurrentEditedText('')
+    }
+  }
+
+  return (
+    <div className='flex flex-col pt-[150px] justify-center bg-gray-50'>
+      <h1>Todo List</h1>
+      <div className='mt-6'>
+        <div className='flex flex-col gap-3'>
+          <input type="text" placeholder="Enter your task..." className="border p-3 flex-1 rounded text-sm" value={text} onChange={e => setText(e.target.value)} onKeyDown={e => e.key === 'Enter' && addTodo()} />
+          <button onClick={addTodo} className="bg-black text-white cursor-pointer">Add</button>
+        </div>
+
+        <ul className='space-y-2 mt-5'>
+          {todoList.map(todo => (
+            <li key={todo.id} className='flex items-center gap-2 p-2 border rounded'>
+              <input type='checkbox' checked={todo.done} onChange={() => dispatch({ type: TYPES.TOGGLE, id: todo.id })} />
+              {currentEditedId === todo.id ? (
+                <div className='flex gap-3'>
+                  <input className='flex-1 text-sm border p-1 rounded' value={currentEditedText} onChange={e => setCurrentEditedText(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && saveEdit()} />
+                  <button onClick={saveEdit} className='bg-black text-white rounded p-2 font-bold cursor-pointer'>Save</button>
+                </div>
+              ) : (
+                <div className='flex justify-between items-center w-full'>
+                  <p className={`text-sm ${todo.done ? "line-through text-gray-500" : ""}`}>{todo.text}</p>
+                  <div className='flex gap-2'>
+                    <Edit onClick={() => {
+                      setCurrentEditedId(todo.id);
+                      setCurrentEditedText(todo.text);
+                    }} className='cursor-pointer' />
+                    <Trash onClick={() => dispatch({ type: TYPES.DELETE, id: todo.id })} className='cursor-pointer' />
+                  </div>
+                </div>
+              )}
+
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default TodoList
